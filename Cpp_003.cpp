ab1e91df7df7480a606fce3a4c61eb167033cf5c
kriti <kritijulia@gmail.com>
2023-08-02 00:36:44 +0530

    simple addition

---
+++ Cpp_003.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"enter first number : ";
+    cin>>a;
+    cout<<"enter second number : ";
+    cin>>b;
+    c=a+b;
+    cout << "sum of " << a << " and " << b << " is " <<c;
+   
+    return 0;
+}
