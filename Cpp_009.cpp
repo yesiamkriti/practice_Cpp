e5be59441a545989f0f2c2fa9359c0f6c533af79
kriti <kritijulia@gmail.com>
2023-08-05 13:53:50 +0530

    bits/stdc++.h

---
+++ Cpp_009.cpp
@@ -0,0 +1,92 @@
+// #include <iostream>
+using namespace std;
+#include <bits/stdc++.h>
+
+// struct janki {
+//     string name = "kriti";
+//     int age = 19;
+// };
+
+// // string GetInfo(string name,int age,string hobbies){
+// //     string info = "name of the person = " + name + "and having " + to_string(age) + " love to do " + hobbies;
+// //     return info;
+// // }
+// // int add(int a ,int b){
+// //     return a+b;
+// // }
+// // void add(int a ,int b,int c){
+// //     cout << a+b+c;
+// // }
+// int main(){
+//     // cout<< add(9.5,4.3);
+//     // cout<< add(9,4) << endl;
+//     // add(9.5,4.3,5);
+
+
+//     // string mssg =GetInfo("kriti",19, "hard work");
+//     // cout<< mssg;
+
+//     janki a;
+//     a = NULL;
+//     cout << a.name;
+
+//     return 0;
+// }
+
+
+//mainpulator in cpp 
+// int main(){
+//     float num = 45.63544;
+//     cout << num << endl;
+//     // cout << setprecision(9) << num;
+//     string myName = "kriti";
+//     // cout << myName << endl;
+//     // cout << setw(5) << myName;
+
+//     cout << "Before " << myName << endl;
+//     cout << "after " << setfill('@') << setw(11);
+//     cout << myName ;
+//     // cout << setbase(16) << 200 << endl;
+//     // setprecision()
+//     return 0 ;
+//     // cout  << insertion
+//     //  cin >> extration
+// }
+class student public:
+int id; string name; double fees;
+student () //constructor
+private : 
+string getInfo (){
+    return ("name = " + name +"having id = " + to_string(id) + "have to pay = " + to_string(fees) );
+}
+int main(){
+    student s1;
+    cout << 
+    // specifier = private , public
+
+    return 0;
+    
+}
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// class Student{
+// public :
+// int id;
+// string name;
+// double fees;
+// Student(){
+// id = 12;
+// name = "Chiper schools";
+// fees = 1.1;
+// }
+// string getInfo(){
+// return ("Name = " + name + " having id = " + to_string(id) + " have to pay = " + to_string(fees));
+// }
+// };
+
+// int main(){
+// Student s1;
+
+// cout << s1.getInfo();
+// }
