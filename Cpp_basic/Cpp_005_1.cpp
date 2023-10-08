3299210571ea44cc9743b36ab8cc42292dc98799
kriti <kritijulia@gmail.com>
2023-10-08 22:51:55 +0530

    for loop

---
+++ Cpp_basic/Cpp_005_1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int x;
+    // int a[5];
+    int a,b,c,d,e;
+    cout<<"Give 5 integers as input"<<endl;
+    cin>>a>>b>>c>>d>>e;
+    cout<<"the integers in reverse order are: "<<e<<d<<c<<b<<a;
+}
