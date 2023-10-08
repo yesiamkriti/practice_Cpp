a825d0d7b0b50ce2aad583c64405ee0a394fcb63
kriti <kritijulia@gmail.com>
2023-10-08 22:05:30 +0530

    primitive data types

---
+++ Cpp_basic/Cpp_002.cpp
@@ -0,0 +1,24 @@
+// data type 
+
+#include<iostream>
+using namespace std;
+int main(){
+    int integer =10;
+    float b = 10.343;
+    string str ="my name is kriti.";
+    char character = 'A';
+    long int c = 100000000000000;
+    bool bool1 = true;
+    bool bool2 = false;
+    unsigned int un_int1 = 0;
+    unsigned int un_int2 =  65535;
+    short int s_int1 = -32768 ;
+    short int s_int1 = 32767 ;
+    long long int;
+    unsigned long long int;
+    signed char;
+    unsigned char;
+    double;
+    long double;
+    return 0;
+}
