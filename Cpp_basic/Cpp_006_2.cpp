b8f93714ee815506083078cee86c659623c23ce0
kriti <kritijulia@gmail.com>
2023-10-08 22:56:20 +0530

    more about for loop

---
+++ Cpp_basic/Cpp_006_2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //  int i;
+    //  i=0;
+    //  int j,k;
+    //  j=0;
+    //  k=0;
+    // for(i=0; ;){
+    //     cout<<i<<" ";
+    // }
+    // cout<<i<<" "<<j<<" "<<k<<endl;
+    // j=i++;
+    // cout<<i<<" "<<j<<" "<<k<<endl;
+    // k=++i;
+    // cout<<i<<" "<<j<<" "<<k<<endl;
+    // cout<<++i<<endl;
+    // cout<<i++;
+    // cout<<i;
+    int i;
+    for(i=1;i<101;i++){
+        cout<<i<<" ";
+    }
+}
